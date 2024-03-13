dc108d11d84180231c5c80fedad7fcfda5fe6254
14790897 <14790897abc@gmail.com>
2024-03-13 15:24:57 +0800

    Update version

--- index_passage_list.js
+++ index_passage_list.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto Read
 // @namespace    http://tampermonkey.net/
-// @version      0.8
+// @version      1.0
 // @description  自动刷linuxdo文章
 // @author       liuweiqing
 // @match        https://linux.do/*

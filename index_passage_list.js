edf2da19ff940dda6ecfa28c851ecd1e9221630f
liuweiqing <liuweiqing147@gmail.com>
2024-04-09 10:49:07 +0800

    chore: version

--- index_passage_list.js
+++ index_passage_list.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto Read
 // @namespace    http://tampermonkey.net/
-// @version      1.2.3
+// @version      1.2.4
 // @description  自动刷linuxdo文章
 // @author       liuweiqing
 // @match        https://linux.do/*

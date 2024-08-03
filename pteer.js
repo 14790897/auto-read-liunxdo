b5f02b44c4b2c1d02d1ac83d1bcfe8d2f2e55096
liuweiqing <liuweiqing147@gmail.com>
2024-08-03 19:00:37 +0800

    fix: es6 dirname不存在

--- pteer.js
+++ pteer.js
@@ -3,6 +3,8 @@ import path from "path";
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
 puppeteer.use(StealthPlugin());
 
@@ -191,7 +193,10 @@ async function launchBrowserForUser(username, password) {
 
     //真正执行阅读脚本
     // 读取外部脚本文件的内容
-    const externalScriptPath = path.join(__dirname, "external.js");
+    const externalScriptPath = path.join(
+      dirname(fileURLToPath(import.meta.url)),
+      "external.js"
+    );
     const externalScript = fs.readFileSync(externalScriptPath, "utf8");
 
     // 在每个新的文档加载时执行外部脚本

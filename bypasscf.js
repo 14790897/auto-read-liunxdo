976d6e43d10065d728e6128eb0b1526bb8b1e9f6
liuweiqing <liuweiqing147@gmail.com>
2024-08-06 09:41:24 +0800

    docs: ...

--- bypasscf.js
+++ bypasscf.js
@@ -228,7 +228,10 @@ async function login(page, username, password) {
       page.click("#login-button"), // 点击登录按钮触发跳转
     ]); //注意如果登录失败，这里会一直等待跳转，导致脚本执行失败 这点四个月之前你就发现了结果今天又遇到（有个用户遇到了https://linux.do/t/topic/169209/82），但是你没有在这个报错你提示我8.5
   } catch (error) {
-    console.error("Navigation timed out in login.请检查用户名是否正确(注意密码中是否有特殊字符,需要外面加上双引号指明这是字符串):", error);
+    console.error(
+      "Navigation timed out in login.请检查用户名密码是否正确(注意密码中是否有特殊字符,需要外面加上双引号指明这是字符串，如果密码里面有双引号则需要转义):",
+      error
+    );
     throw new Error("Navigation timed out in login.");
   }
   await delayClick(1000);

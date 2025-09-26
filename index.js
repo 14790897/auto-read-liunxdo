c0cb2e6f038f78145051679b43da44656018c9aa
liuweiqing <liuweiqing147@gmail.com>
2025-09-26 09:57:30 +0800

    version 1.4.5

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto Read
 // @namespace    http://tampermonkey.net/
-// @version      1.4.4
+// @version      1.4.5
 // @description  自动刷linuxdo文章
 // @author       liuweiqing
 // @match        https://meta.discourse.org/*
